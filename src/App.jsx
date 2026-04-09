ac99246ef49fb1a7bcb475b642758aee412e6750
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-04-09 12:16:36 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
 import Pricing from "./components/Pricing";
+import { useState } from "react";
 
 function App() {
+  const [scrolled, setScrolled] = useState(false);
 
+  useEffect(() => {
+
+    function handleScroll() {
+      setScrolled(window.scrollY > 50)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.addEventListener('scroll', handleScroll)
+  }, [])
   return (
     <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
       <Navbar/>
